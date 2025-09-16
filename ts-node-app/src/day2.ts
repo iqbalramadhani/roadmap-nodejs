63b65dbe6cd63d36a0a8cc775001a65efb8ddfb1
Iqbal Ramadhani <iqbal.ramadhani55@gmail.com>
2025-09-16 11:00:55 +0700

    type script learn

---
+++ ts-node-app/src/day2.ts
@@ -0,0 +1,25 @@
+let username: string = "iqbal";
+console.log("Username: ", username);
+
+// number
+let age: number = 25;
+let priice: number = 19999
+console.log("Age: ", age, ", price: ", priice);
+
+// BOOLEAN
+let isActive: boolean = true;
+console.log("Active:",isActive);
+
+// array
+let scores: number[] = [90, 95, 80, 85, 75];
+console.log("Scores: ", scores);
+
+// array generic
+let fruits: Array<string> = ["apple", "banana", "orange"];
+console.log("Fruits: ", fruits);
+
+// TUPLE (array dengan panjang & tipe tetap)
+let user: [string, number, boolean] = ["iqbal", 25, true];
+console.log("User: ", user);
+
+
